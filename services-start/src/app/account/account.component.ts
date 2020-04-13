c98276bd90ee0b9cb2079fde48a0c2353574f7d1
hareshbhai <haresh.prajapati@outlook.com>
2020-04-13 22:59:40 +0530

    Depency injection added for Logging service

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -4,19 +4,23 @@ import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private loggingService : LoggingService){
+  }
+
 
   onSetTo(status: string) {
 
     this.statusChanged.emit({id: this.id, newStatus: status});
     const service = new LoggingService();
-    service.logStatusChange(status);
+    this.loggingService.logStatusChange(status);
     
     //console.log('A server status changed, new status: ' + status);
   }
--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -4,10 +4,15 @@ import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private logginService: LoggingService){
+
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
@@ -16,7 +21,7 @@ export class NewAccountComponent {
     });
 
     const service = new LoggingService();
-    service.logStatusChange(accountStatus);
+    this.logginService.logStatusChange(accountStatus);
     //console.log('A server status changed, new status: ' + accountStatus);
   }
 }
